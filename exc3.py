134861783fb154bd1744f56c26a781524eb1337f
tilenkosec <hiperflash@gmail.com>
2018-05-23 09:45:12 +0200

    added 4th

--- exc3.py
+++ exc3.py
@@ -4,22 +4,21 @@ import helper3
 # Save input into variable named your_input.
 # If user inputs a number greater than 40, set your_input to 0.
 
-your_input = input("How many groceries do you want to buy?: ")
+your_input = int(input("How many groceries do you want to buy?: "))
 if your_input > 40:
     your_input = 0
-print your_input
+    print your_input
 
 # Do not change line below.
 price_list, groceries_list, money_available = helper3.buy(your_input)
 #
 
 # 2. Create a function that accepts price_list as a parameter and returns average.
-def avg (price_list): 
+def avgg(price_list): 
     average = (sum(price_list) / len(price_list))
     return average
-
-average = avg(price_list)
-print average
+average = avgg(price_list)
+print "This is the average price:", average
 
 print price_list, groceries_list
 # 3. price_list and groceries_list are arrays that are the same length. groceries_list contains names of groceries. First price in
@@ -28,12 +27,16 @@ print price_list, groceries_list
 # food, 15
 # clothes, 9
 # ...
-def list_all (): 
-    if cost:
-    foodcount = foodcount+1
-    return foodcount
-    elif 
 
+def combine(groceries_list, price_list):
+    for i in range(len(groceries_list)):
+        print(str(groceries_list[i]) + ", " + (str(price_list[i]))
+#combine(groceries_list, price_list)
 
 # 4. Use a function from task #2 and for every grocery prints if its price is above or below average.
-print "This items are over average" avg()
+
+for i in price_list:
+    if (avgg(price_list)>i):
+    print ("This one is above the average price: ", i)
+    elif (avgg(price_list)<i):
+    print ("This one is above the below price: ", i)
